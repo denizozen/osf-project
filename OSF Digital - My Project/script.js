832057f55ca551fbbc17738e06d95a60d53802d7
denizozen <68877984+denizozen@users.noreply.github.com>
2022-08-18 15:36:13 +0300

    Add files via upload

---
+++ OSF Digital - My Project/script.js
@@ -0,0 +1,27 @@
+/* footer buttons */
+function myFunction() {
+  let x = document.getElementById("contact-info");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction2() {
+  let x = document.getElementById("categories-info");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+
+function myFunction3() {
+  let x = document.getElementById("about-info");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
